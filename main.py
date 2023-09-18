316659a0f5595ffa6f89322abb61507e209216ab
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-18 18:14:11 +0500

    sep 18 1

--- main.py
+++
@@ -1,7 +0,0 @@
-def per(obt, tot):
-    return obt/tot*100
-
-result = per(obt = 50, tot = 100)
-
-if result > 50:
-    print("You have passed the exam")
---
+++ sep_18_23/main.py
@@ -0,0 +1,10 @@
+def hello():
+    print("hello world")
+
+def hi(name):
+    print('Hi ' + name)
+
+def hello1(name):
+    return 'Hello ' + name
+
+result = hello1('Bob')
