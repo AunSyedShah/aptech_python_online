b3d8b45d4cb570b3eafd8f84c88bfad7b5c38dd4
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-20 17:53:04 +0500

    instructions about libraries

--- sep_19_203/libraries.py
+++ sep_19_203/libraries.py
@@ -1,9 +1,3 @@
-# print(16 ** (1/2)) - square root
-# library is a pre-written code
-import math
+import math # always import every library on top of the file
 result = math.degrees(1)
 print(result)
-
-import math
-result = math.factorial(20)
-print (result)
