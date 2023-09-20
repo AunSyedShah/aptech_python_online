98e8acf5bc8ae4d383c04f612b9cd9d02826735e
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-20 18:44:34 +0500

    list loop application

--- sep_19_203/lists.py
+++ sep_19_203/lists.py
@@ -1,18 +1,7 @@
-# without list
-frui1 = 'apple'
-frui2 = 'banana'
-frui3 = 'orange'
-frui4 = 'grape'
-frui5 = 'mango'
-# with list
-fruits = ['apple', 'banana', 'orange', 'grape', 'mango']
-person_ages = ['naruto', 20, 'sasuke', 21, 'sakura', 20]
-print(fruits[4])
-# negative index
-print(fruits[-1])
-# range of index
-print(fruits[2:5])
-# skip start index
-print(fruits[:4])
-# skip end index
-print(fruits[2:])
+total = 0
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in numbers:
+    if (i % 2 == 0):
+        total = total + i
+
+print(total)
