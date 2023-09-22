ab176c30f9a6a8835babc4cbca9b41c6109db688
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-22 17:30:14 +0500

    length using method

--- sep_22_23/list_methods.py
+++ sep_22_23/list_methods.py
@@ -1,7 +1,11 @@
 # sum using loop
-total = 0
-marks = [90, 25, 67, 45, 80]
-for i in marks:
-    total += i
+# total = 0
+# marks = [90, 25, 67, 45, 80]
+# for i in marks:
+#     total += i
 
-print(f"Total: {total}")
+# print(f"Total: {total}")
+
+marks = [90, 25, 67, 45, 80]
+length_of_marks = len(marks)
+print(length_of_marks)
