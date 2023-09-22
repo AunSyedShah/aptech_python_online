dae02ba1f861aa244bbdcfda4f72e125f6d9c940
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-22 17:25:39 +0500

    sum using loop

--- sep_20_23/lists.py
+++ sep_20_23/lists.py
@@ -1,7 +1,14 @@
-total = 0
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in numbers:
-    if (i % 2 == 0):
-        total = total + i
+# total = 0
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in numbers:
+#     if (i % 2 == 0):
+#         total = total + i
 
-print(total)
+# print(total)
+
+total = 0
+marks = [90, 25, 67, 45, 80]
+for i in marks:
+    total += i
+
+print(f"Total: {total}")
