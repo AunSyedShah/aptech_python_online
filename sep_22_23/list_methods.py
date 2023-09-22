26a851c19d19e2a4428acb647dfa4ef178bf904f
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-22 17:45:25 +0500

    append method

--- sep_22_23/list_methods.py
+++ sep_22_23/list_methods.py
@@ -12,7 +12,13 @@
 # print(length_of_marks)
 
 # sum of marks using built-in function
-marks = [90, 25, 67, 45, 80]
-sum_of_marks = sum(marks)
-print(sum_of_marks) 
-print(  sum(marks)    ) # same as above
+# marks = [90, 25, 67, 45, 80]
+# sum_of_marks = sum(marks)
+# print(sum_of_marks) 
+# print(  sum(marks)    ) # same as above
+
+# python list append method
+# marks = [90, 25, 67, 45, 80]
+# print(marks)
+# marks.append(100)
+# print(marks)
