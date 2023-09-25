bb7473c370e7fbae0eb5146be68f86955b4cc9da
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-09-25 18:44:45 +0500

    contact list delete

--- sep_25_23/app.py
+++ sep_25_23/app.py
@@ -8,6 +8,11 @@ def add_contact_hadnler():
     conact_number_value = contact_number_entry.get()
     contacts.append(conact_number_value)
 
+def view_contact_handler():
+    contact_text_box.delete(1.0, tk.END)
+    for contact in contacts:
+        contact_text_box.insert(tk.END, contact + "\n")
+
 root_window.geometry("1250x500")
 root_window.minsize(500, 150)
 root_window.title("My First GUI Program")
@@ -20,5 +25,12 @@ contact_number_entry.pack()
 
 contact_button = tk.Button(root_window, text="Add Contact", command=add_contact_hadnler)
 contact_button.pack()
+
+view_contact_button = tk.Button(root_window, text="View Contact", command=view_contact_handler)
+view_contact_button.pack()
+
+
+contact_text_box = tk.Text(root_window, height=10, width=50)
+contact_text_box.pack()
 
 root_window.mainloop()
