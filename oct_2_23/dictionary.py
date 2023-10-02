483795210830517d8c3b4a187785b24543c11a01
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-10-02 17:57:32 +0500

    dictionay modify

--- oct_2_23/dictionary.py
+++ oct_2_23/dictionary.py
@@ -5,4 +5,17 @@ person = {
     "address": {"street": "123 street", "city": "tehran", "country": "iran"},
     "is_male": True,
 }
+# print dictionary
 print(person)
+# access a key
+print(person["name"])
+# add an element to dictionary
+person["job"] = "programmer"
+# print to verify
+print(person)
+# delete an element from dictionary
+del person["job"]
+# access list inside dictionary
+print(person["skills"][1])
+# access dictionary inside dictionary
+print(person["address"]["city"])
