d15fb1d8be6dd625e75d95644a57a600d3f2e5d4
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-10-02 17:42:37 +0500

    dictionary

---
+++ oct_2_23/dictionary.py
@@ -0,0 +1,8 @@
+person = {
+    "name": "taha",
+    "age": 30,
+    "skills": ["python", "javascript", "c++", "c#"],
+    "address": {"street": "123 street", "city": "tehran", "country": "iran"},
+    "is_male": True,
+}
+print(person)
