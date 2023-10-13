22625d58a916a2e8d17d05b8337d85d8b424b8cb
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-10-13 17:42:27 +0500

    create_contact_button

--- oct_11_23/gui.py
+++ oct_11_23/gui.py
@@ -3,14 +3,26 @@ import tkinter as tk
 
 # step 1 - create root window
 root = tk.Tk()
+def create_contact():
+    print("Create Contact")
+
+
+
 root.title("My First GUI App")
 root.geometry("750x250")
 
 # step 3 - create widgets
-label = tk.Label(root, text="Hello, World!")
+label = tk.Label(root, text="Contact App")
 label.pack()
-login_button = tk.Button(root, text="Login")
-login_button.pack()
+
+create_button = tk.Button(root, text="Create Contact", command=create_contact)
+create_button.pack()
+view_button = tk.Button(root, text="View Contact")
+view_button.pack()
+update_button = tk.Button(root, text="Update Contact")
+update_button.pack()
+delete_button = tk.Button(root, text="Delete Contact")
+delete_button.pack()
 
 
 # step 2 - create mainloop
