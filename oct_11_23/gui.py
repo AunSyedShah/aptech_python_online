c229cae7d006cf9cd0fcd287aaf70d5109abbdab
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-10-13 18:10:26 +0500

    create_new_contact_window

--- oct_11_23/gui.py
+++ oct_11_23/gui.py
@@ -3,8 +3,24 @@ import tkinter as tk
 
 # step 1 - create root window
 root = tk.Tk()
+
 def create_contact():
-    print("Create Contact")
+    # open a new window
+    create_contact_window = tk.Toplevel(root)
+    create_contact_window.title("Create Contact")
+    create_contact_window.geometry("250x250")
+    # two labels and two entry fields for contact_name and contact_number
+    name_label = tk.Label(create_contact_window, text="Contact Name")
+    name_label.pack()
+    name_entry = tk.Entry(create_contact_window)
+    name_entry.pack()
+    number_label = tk.Label(create_contact_window, text="Contact Number")
+    number_label.pack()
+    number_entry = tk.Entry(create_contact_window)
+    number_entry.pack()
+    # create a button to save the contact
+    save_button = tk.Button(create_contact_window, text="Save Contact")
+    save_button.pack()
 
 
 
@@ -21,6 +37,7 @@ view_button = tk.Button(root, text="View Contact")
 view_button.pack()
 update_button = tk.Button(root, text="Update Contact")
 update_button.pack()
+# delete_button with styles
 delete_button = tk.Button(root, text="Delete Contact")
 delete_button.pack()
 
