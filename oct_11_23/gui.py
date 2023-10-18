fcf158b475d3d5796c805208a2b0c5b7c323bd9a
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-10-18 17:48:24 +0500

    contact_ui

--- oct_11_23/gui.py
+++ oct_11_23/gui.py
@@ -6,30 +6,7 @@ from tkinter import messagebox
 root = tk.Tk()
 
 def create_contact():
-    # save contact function handler for save_button, to display a gui message box
-    def save_contact():
-        # get the contact name and number
-        contact_name = name_entry.get()
-        contact_number = number_entry.get()
-        print(contact_name, contact_number)
-
-
-    # open a new window
-    create_contact_window = tk.Toplevel(root)
-    create_contact_window.title("Create Contact")
-    create_contact_window.geometry("250x250")
-    # two labels and two entry fields for contact_name and contact_number
-    name_label = tk.Label(create_contact_window, text="Contact Name")
-    name_label.pack()
-    name_entry = tk.Entry(create_contact_window)
-    name_entry.pack()
-    number_label = tk.Label(create_contact_window, text="Contact Number")
-    number_label.pack()
-    number_entry = tk.Entry(create_contact_window)
-    number_entry.pack()
-    # create a button to save the contact
-    save_button = tk.Button(create_contact_window, text="Save Contact", command=save_contact)
-    save_button.pack()
+    print("Create Contact")
 
 
 
@@ -49,6 +26,10 @@ update_button.pack()
 # delete_button with styles
 delete_button = tk.Button(root, text="Delete Contact")
 delete_button.pack()
+
+# big text box
+text_box = tk.Text(root, height=5, width=50)
+text_box.pack()
 
 
 # step 2 - create mainloop
